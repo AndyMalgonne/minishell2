7be671afd056b3e4db504747828fd45d15e70756
Neffi42 <114996380+Neffi42@users.noreply.github.com>
2025-01-27 13:11:56 +0100

    create data structures

--- include/minishell.h
+++ include/minishell.h
@@ -27,4 +27,34 @@
 # include <sys/types.h>
 # include <termios.h>
 
+typedef enum e_iotype
+{
+	INFILE,
+	HEREDOC,
+	OUTFILE_TRUNC,
+	OUTFILE_APPEND,
+}					t_iotype;
+
+typedef struct s_env
+{
+	char			*value;
+	struct s_env	*next;
+}					t_env;
+
+typedef struct s_iofile
+{
+	t_iotype		type;
+	char			*value;
+	struct s_iofile	*next;
+}					t_iofile;
+
+typedef struct s_tree
+{
+	char			*cmd;
+	t_iofile		*io;
+	struct s_tree	*next;
+}					t_tree;
+
+extern int			g_signal;
+
 #endif
