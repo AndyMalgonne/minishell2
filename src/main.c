c30874a22b1daa2ad99ec95b57aa29d9ec7ea28b
Neffi42 <114996380+Neffi42@users.noreply.github.com>
2025-01-27 19:40:21 +0100

    create error messages for create_env and isatty case

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: abasdere <abasdere@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/27 09:50:52 by andymalgonn       #+#    #+#             */
-/*   Updated: 2025/01/27 19:09:00 by abasdere         ###   ########.fr       */
+/*   Updated: 2025/01/27 19:38:53 by abasdere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ int	main(int argc, char **argv __attribute__((unused)), char **envp)
 	if (argc != 1)
 		return (ft_putstr_fd("Usage: ./minishell\n", 2), 1);
 	if (isatty(0) != 1)
-		return (printf("U mad bro?\n"));
+		return (ft_putstr_fd("U mad bro?\n", 2), 1);
 	if (!create_env(&env, envp))
 		return (1);
 	return (0);
