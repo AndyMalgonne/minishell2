f33017bc7f732c06ecc9684154c8a755350e8992
Neffi42 <114996380+Neffi42@users.noreply.github.com>
2025-01-29 09:52:14 +0100

    norm free_to_null

--- src/utils.c
+++ src/utils.c
@@ -6,18 +6,19 @@
 /*   By: abasdere <abasdere@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/28 10:02:20 by abasdere          #+#    #+#             */
-/*   Updated: 2025/01/28 14:08:49 by abasdere         ###   ########.fr       */
+/*   Updated: 2025/01/29 09:51:39 by abasdere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void *free_to_null(void *p) {
-	void **ptr;
+void	*free_to_null(void *p)
+{
+	void	**ptr;
 
 	ptr = (void **)p;
 	if (!ptr || !(*ptr))
-		return NULL;
+		return (NULL);
 	free(*ptr);
 	*ptr = NULL;
 	return (*ptr);
